de6ff92b21e9fb248d38e6de73482995ae629b36
bluessista <inga@bluessista.com>
2019-03-10 12:17:52 +0100

    feat: add colors for styling

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+// add HEX codes though variables can be used in the style components
+export const purple = '#292477';
+export const gray = '#757575';
+export const white = '#fff';
+export const red = '#b71845';
+export const orange = '#f26f28';
+export const blue = '#4e4cb8';
+export const lightPurp = '#7c53c3';
+export const pink = '#b93fb3';
+export const green = '#a4c639';
