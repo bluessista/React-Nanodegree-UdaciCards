c31b6ad2e8ffe83f4cc32829c0c04300d87ad828
bluessista <inga@bluessista.com>
2019-03-16 21:54:21 +0100

    feat: add StackNavigator to navigate to DeckDetails

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 import {orange, white, gray } from './utils/colors';
 
 import DeckList from './components/DeckList';
 import AddNewDeck from './components/AddNewDeck';
+import DeckView from './components/DeckView';
 
 class App extends Component {
     render() {
       return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <DeckList />
-          <Tabs />
+          <RootStack />
         </View>
       );
     }
@@ -30,7 +31,7 @@ const NavigatorTabs = createBottomTabNavigator({
     AddNewDeckView: {
         screen: AddNewDeck,
         navigationOptions: {
-            tabBarLabel: 'Add new deck',
+            tabBarLabel: 'Add new Deck',
             tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='library-plus' size={25} color={tintColor} />
         }
     }
@@ -43,4 +44,28 @@ const NavigatorTabs = createBottomTabNavigator({
     }
 })
 
-export default createAppContainer(NavigatorTabs);
+const RootStack = createStackNavigator(
+    {
+        Home: {
+            screen: NavigatorTabs,
+            navigationOptions: {
+                header: null
+            }
+        },
+        DeckDetails: {
+            screen: DeckView,
+            navigationOptions: {
+                title: 'Deck Details',
+                headerTintColor: white,
+                headerStyle: {
+                    backgroundColor: orange
+                }
+            }
+        }
+    },
+    {
+        initialRouteName: 'Home'
+    }
+)
+
+export default createAppContainer(RootStack);
