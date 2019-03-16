5f421e9f93b8df985bea02c99ce4ee65ce0643b7
bluessista <inga@bluessista.com>
2019-03-16 19:08:18 +0100

    add Tab navigation to Deck List View in App

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import DeckList from './components/DeckList'
+import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import {orange, white, gray } from './utils/colors';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <DeckList />
-      </View>
-    );
+import DeckList from './components/DeckList';
+import AddNewDeck from './components/AddNewDeck';
+
+class App extends Component {
+    render() {
+      return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <DeckList />
+          <Tabs />
+        </View>
+      );
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%'
-  },
-});
+// implement Tab Navigation
+const NavigatorTabs = createBottomTabNavigator({
+    DeckListView: {
+        screen: DeckList,
+        navigationOptions: {
+            tabBarLabel: 'Decks',
+            tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-variant' size={25} color={tintColor} />
+        }
+    },
+    AddNewDeckView: {
+        screen: AddNewDeck,
+        navigationOptions: {
+            tabBarLabel: 'Add new deck',
+            tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='library-plus' size={25} color={tintColor} />
+        }
+    }
+}, {
+    tabBarOptions: {
+        activeTintColor: orange,
+        style: {
+            backgroundColor: white
+        }
+    }
+})
+
+export default createAppContainer(NavigatorTabs);
