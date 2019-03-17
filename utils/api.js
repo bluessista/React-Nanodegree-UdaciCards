441ba06dfd14335a4ddb50c75056ed82ec8dd1e7
bluessista <inga@bluessista.com>
2019-03-17 18:02:18 +0100

    refactor: update getDecks method, return dummyData

--- utils/api.js
+++ utils/api.js
@@ -1,42 +1,48 @@
 import { AsyncStorage } from 'react-native';
-import { FLASHCARD_STORAGE_KEY } from './helpers'
+import { UDACICARD_STORAGE_KEY } from './helpers';
 
 // basic data
 const dummyData = {
     React: {
-      title: 'React',
-      questions: [
-        {
-          question: 'What is React?',
-          answer: 'A library for managing user interfaces'
-        },
-        {
-          question: 'Where do you make Ajax requests in React?',
-          answer: 'The componentDidMount lifecycle event'
-        }
-      ]
+        title: 'React',
+        questions: [
+            {
+                question: 'What is React?',
+                answer: 'A library for managing user interfaces',
+                correctAnswer: 'true'
+            },
+            {
+                question: 'Where do you make Ajax requests in React?',
+                answer: 'The componentDidMount lifecycle event',
+                correctAnswer: 'true'
+            }
+        ]
     },
     JavaScript: {
-      title: 'JavaScript',
-      questions: [
-        {
-          question: 'What is a closure?',
-          answer: 'The combination of a function and the lexical environment within which that function was declared.'
-        }
-      ]
+        title: 'JavaScript',
+        questions: [
+            {
+                question: 'What is a closure?',
+                answer: 'The combination of a function and the lexical environment within which that function was declared.',
+                correctAnswer: 'true'
+            }
+        ]
     }
-  }
+};
 
 // getDecks: return all of the decks along with the titles
 export const getDummyData = () => {
     return dummyData;
-}
+};
 
-export function getDecks () {
-    return AsyncStorage.getItem(FLASHCARD_STORAGE_KEY)
-       .then((results) => {
-           return JSON.parse(results);
-     });
+export function getDecks (deck) {
+    return AsyncStorage.getItem(UDACICARD_STORAGE_KEY)
+        .then(results => {
+            if(results === null){
+                AsyncStorage.setItem(UDACICARD_STORAGE_KEY, JSON.stringify(dummyData));
+                return dummyData;
+            } else{
+                return JSON.parse(results);
+            }
+        });
 }
-
-// getDeck: take in a single id argument and return the deck associated with that id
